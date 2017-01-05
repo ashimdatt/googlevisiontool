432cf6b8a722b00791d760372e403fa49e9494ea
ashimdatt <datta.ashim2@gmail.com>
2017-01-04 18:06:11 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+
+import matplotlib
+
+matplotlib.use('Agg')
+
+import sys
+import os
+import datetime
+import unicodedata as ucd
+
+from flask import Flask, render_template, request, redirect, url_for, abort, session,flash
+from flask.ext.session import Session
+from redis import Redis
+
+import pickle
+import json
+
+import random
+import psycopg2
+
+app = Flask(__name__)
+app.debug = True
+app.config['SESSION_TYPE'] = 'redis'
+Session(app)
+
+@app.route('/')
+def index():
+    #for key in session.keys():
+    #    session.pop(key, None)
+    #session.clear()
+    #session.new = True
+    #session['sessionid'] = os.urandom(24)
+    #session['count'] = 1
+    return redirect( url_for('image_chooser') )
+
+@app.route('/image_chooser',methods=['GET'])
+def image_chooser():
+
+    return render_template('imagebrowser.html')
+
+@app.route('/image_results',methods=['POST'])
+def image_results():
+
+    session['eventid'] = request.form['eventid']
+    session['keyword'] = request.form['keyword']
+
+    image_url_list = fetch_images(session['eventid'],session['keyword'])
+
+    if image_url_list == -999:
+        return "Ben doesn't know SQL."
+    
+    return render_template('image_results.html',image_url_list=image_url_list)
+
+def fetch_images(eventid,keyword):
+
+    sql_query = """
+                SELECT distinct 'https://d3dhqk2br2olrw.cloudfront.net/' || LOWER(ucii.externalimageid) || '.jpg' AS ImageURL
+                FROM PUBLIC.Ratings_UserCheckIns UCI
+                JOIN PUBLIC.Ratings_UserCheckInImages UCII
+                ON UCI.CheckInId = UCII.CheckInId
+                join jt.compute_vision_hackday b
+                on UPPER(UCI.Applicationid)=upper(b.applicationid)
+                and LOWER(ucii.externalimageid)=lower(b.imagefilename)
+                WHERE UCI.ApplicationId = upper('%s')
+                and 
+               ( 
+               ((lower((LabelAnnotations->>0)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>0)::JSONB->>'score')as decimal)>.5)
+                or ((lower((LabelAnnotations->>1)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>1)::JSONB->>'score')as decimal)>.5)
+                or ((lower((LabelAnnotations->>2)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>2)::JSONB->>'score')as decimal)>.5)
+                or ((lower((LabelAnnotations->>3)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>3)::JSONB->>'score')as decimal)>.5)
+                or ((lower((LabelAnnotations->>4)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>4)::JSONB->>'score')as decimal)>.5)
+                or ((lower((LabelAnnotations->>5)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>5)::JSONB->>'score')as decimal)>.5)
+                or ((lower((LabelAnnotations->>6)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>6)::JSONB->>'score')as decimal)>.5)
+                or ((lower((LabelAnnotations->>7)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>7)::JSONB->>'score')as decimal)>.5)
+                or ((lower((LabelAnnotations->>8)::JSONB->>'description') like lower('%s'))
+                and cast(((LabelAnnotations->>8)::JSONB->>'score')as decimal)>.5)
+                or (lower((textannotations->>0)::JSONB->>'description') like lower('%s'))
+                or (lower((textannotations->>1)::JSONB->>'description') like lower('%s'))
+                or (lower((textannotations->>2)::JSONB->>'description') like lower('%s'))
+                or (lower((textannotations->>3)::JSONB->>'description') like lower('%s'))
+                or (lower((textannotations->>4)::JSONB->>'description') like lower('%s'))
+                or (lower((textannotations->>5)::JSONB->>'description') like lower('%s'))
+                or (lower((textannotations->>6)::JSONB->>'description') like lower('%s'))
+                or (lower((textannotations->>7)::JSONB->>'description') like lower('%s'))
+                or (lower((textannotations->>8)::JSONB->>'description') like lower('%s'))                           
+                )
+                """ % (eventid,("%"+keyword+"%"),("%"+keyword+"%"),("%"+keyword+"%"),
+                ("%"+keyword+"%"),("%"+keyword+"%"),("%"+keyword+"%"),("%"+keyword+"%"),
+                ("%"+keyword+"%"),("%"+keyword+"%"),("%"+keyword+"%"),("%"+keyword+"%"),("%"+keyword+"%"),
+                ("%"+keyword+"%"),("%"+keyword+"%"),("%"+keyword+"%"),("%"+keyword+"%"),
+                ("%"+keyword+"%"),("%"+keyword+"%"))
+    conn = psycopg2.connect("dbname='analytics' user='etl' host='10.223.192.6' password='s0.Much.Data' port='5432'")
+    cur = conn.cursor()
+
+    try:
+        cur.execute(sql_query)
+    except:
+        raise
+        return -999
+
+    entries = map(lambda x: x[0].strip('\n'),cur.fetchall())
+
+    cur.close()
+    conn.close()
+
+    return entries
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
---
+++ google_vision_tool.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import json
+import requests
+import psycopg2
+import datetime
+
+import google_code
+
+def push_to_database(result_json,table_name, applicationId):
+
+    result_json = result_json['responses']
+
+    conn = psycopg2.connect(""dbname='analytics' user='name' host='server' password='pass' port=''"")
+    cur = conn.cursor()
+
+    #print google_code.DETECTION_TYPES
+    
+    for entry in result_json:
+        input_list = []
+
+        # Add Application Id to first field
+        input_list.append(applicationId)
+
+        # Add Current Time
+        current_time = str(datetime.datetime.utcnow())
+        input_list.append(current_time)
+
+        if len(entry.keys()) == 0:
+            continue
+        for key in ['filename','faceAnnotations','labelAnnotations','safeSearchAnnotation','textAnnotations']:
+            if key in entry:
+                input_list.append(json.dumps(entry[key]))
+            else:
+                input_list.append(None)
+
+
+        # Format .jpg filename
+        input_list[2] = input_list[2].split('/',1)[1].split('.')[0].upper()
+                
+        format_string = ','.join(['%s' for i in range(len(input_list))])
+        insert_string = """INSERT INTO """ + table_name + """ VALUES (""" + format_string + """)"""
+         
+        cur.execute(insert_string,tuple(input_list))
+        conn.commit()
+
+    cur.close()
+    conn.close()
+                        
+    return 1
+
+def google_call(payload):
+    
+    #payload = open(filepath, 'rb').read()
+    response = requests.post(url='https://vision.googleapis.com/v1/images:annotate?key=AIzaSyB-Nn689M8B9UKSP6mfrQiKbgDHHfBpr-U',data=json.dumps(payload),headers={'Content-Type': 'application/json'})
+
+    #return json.loads(response.text)
+    return response.text
+
+def build_payload(image_list_file):
+
+    input_lines = file(image_list_file,'r').readlines()
+    google_code.main(input_lines,'payload.json')    
+
+    return json.load(file('payload.json')),map(lambda x: x.split()[0],input_lines)
+
+def main(filepath, tablename, applicationId):
+
+    #if len(sys.argv) > 1:
+    #    filepath = sys.argv[1]
+
+    #if len(sys.argv) > 2:
+    #    tablename = sys.argv[2]
+
+    try:
+        payload,imagepath_list = build_payload(filepath)
+    except:
+        print 'ERROR Unable to build payload for API call. Make sure file exists'
+        raise        
+
+    try:
+        result = google_call(payload)
+        result_json = json.loads(result)        
+        print 'LOG Succesful API call'        
+    except:
+        print 'ERROR Failed API call'
+        raise
+    
+    for i,entry in enumerate(result_json['responses']):
+        entry['filename'] = imagepath_list[i]
+        
+    try:
+        push_to_database(result_json,tablename, applicationId)
+        print 'Insert Successful into DB'
+    except:
+        print 'ERROR Unable to push to database. Make sure table is consistent with input schema'
+        raise
+
+    return 1
+    
+
+if __name__ == "__main__":
+    
+    main()
+
+    
+    
